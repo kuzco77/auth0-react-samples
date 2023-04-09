061576a2e1748eed69be73ca685c2fe2c7fb9e75
kuzco77 <namanhchu2103@gmail.com>
2023-04-10 00:14:39 +0700

    Change docker file

--- Sample-01/exec.sh
+++ Sample-01/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t auth0-react-02-calling-an-api .
-docker run --init -p 3000:3000 -p 3001:3001 -it auth0-react-02-calling-an-api
+docker run --name auth0-react --init -dp 4000:4000 -p 4001:4001 -it auth0-react-02-calling-an-api
